d9a1358ed4d15d130724da42934b9dce2c7e27db
dmika1234 <dmika1234@gmail.com>
2023-06-19 20:07:17 +0200

    eval change

--- Project/all_coco_utils.py
+++ Project/all_coco_utils.py
@@ -88,7 +88,7 @@ def _get_iou_types(model):
 
 
 @torch.no_grad()
-def evaluate(model, data_loader, device):
+def coco_evaluate(model, data_loader, device):
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
     torch.set_num_threads(1)
